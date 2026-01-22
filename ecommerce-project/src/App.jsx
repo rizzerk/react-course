4a3d27ab8c080af6a5f21fd8a2279f9ab8883a68
rizzerk <24101986@usc.edu.ph>
2026-01-22 20:57:57 +0800

    Update links to match the router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path ="/" element={<HomePage />} />
+      <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
 
